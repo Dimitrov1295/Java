799f0456fbc2f1f0e29db1b73b40e210f7ace852
unknown <ivandimitrov9512@gmail.com>
2020-05-06 17:21:27 +0100

    Add files via upload

--- BackgroundChanger/src/test/java/uk/co/ivandimitrov/AppTest.java
+++ BackgroundChanger/src/test/java/uk/co/ivandimitrov/AppTest.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package uk.co.ivandimitrov;
 
 import org.junit.Test;
@@ -16,3 +17,23 @@ public class AppTest {
         assertTrue(true);
     }
 }
+=======
+package uk.co.ivandimitrov;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
+>>>>>>> 2a010a0... Add files via upload
