926db3d1944e4ebd1fdb53da61205c3041b33278
unknown <ivandimitrov9512@gmail.com>
2020-05-06 22:12:08 +0100

    Fixes

--- BackgroundChanger/src/test/java/uk/co/ivandimitrov/AppTest.java
+++ BackgroundChanger/src/test/java/uk/co/ivandimitrov/AppTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package uk.co.ivandimitrov;
 
 import org.junit.Test;
@@ -17,27 +16,3 @@ public class AppTest {
         assertTrue(true);
     }
 }
-=======
-package uk.co.ivandimitrov;
-
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest {
-    /**
-     * Rigorous Test.
-     */
-    @Test
-    public void testApp() {
-        assertTrue(true);
-    }
-}
-<<<<<<< HEAD
->>>>>>> 2a010a0... Add files via upload
-=======
->>>>>>> refs/remotes/origin/master
->>>>>>> 5b2d65917f2d8186a0eaf2263995c748ca2ddaed
